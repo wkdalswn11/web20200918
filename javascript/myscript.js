472634035f8b7bc55a54c09e3e81ae394f72edae
wkdalswn11 <wkdalswn133@naver.com>
2020-09-25 17:10:57 +0900

    javascript, jquery

---
+++ javascript/myscript.js
@@ -0,0 +1,13 @@
+function show() {
+    console.log("show function");
+    }
+
+    function $myfunc() {
+        console.log("my function");
+    }
+
+    function $() {
+        console.log("$ function");
+    }
+
+    
